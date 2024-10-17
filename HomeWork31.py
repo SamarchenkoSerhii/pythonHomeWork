107b03ca0371eb83b6b6c16e19484a3a3707cfa9
Serhii <samarchenkoserhii@gmail.com>
2024-10-17 13:18:23 +0200

    HomeWork

---
+++ HomeWork31.py
@@ -0,0 +1,16 @@
+digit1  = int(input("Please enter a first  number: "))
+digit2  = int(input("Please enter a second number: "))
+operation  = input("Please select operation +, -, / or *  :")
+if operation == "+" :
+    print(digit1,operation,digit2, "=",digit1 + digit2)
+elif operation == "-" :
+    print(digit1,operation,digit2, "=",digit1 - digit2)
+elif operation == "*":
+    print(digit1,operation,digit2, "=",digit1 * digit2)
+elif operation == "/" and digit2 != 0:
+    print(digit1,operation,digit2, "=",digit1 / digit2)
+else:
+    print("incorrect input data")
+    if operation == "/" and digit2 == 0:
+        print("second number is zero")
+
