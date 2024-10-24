90587c910549d3ee446a206132a5759dcb31491e
Serhii <samarchenkoserhii@gmail.com>
2024-10-25 00:45:53 +0200

    HomeWork

---
+++ HomeWork52.py
@@ -0,0 +1,18 @@
+WhileY = "y"
+while WhileY == "y":
+    digit1  = int(input("Please enter a first  number: "))
+    digit2  = int(input("Please enter a second number: "))
+    operation  = input("Please select operation +, -, / or *  :  ")
+    if operation == "+" :
+        print(digit1,operation,digit2, "=",digit1 + digit2)
+    elif operation == "-" :
+        print(digit1,operation,digit2, "=",digit1 - digit2)
+    elif operation == "*":
+        print(digit1,operation,digit2, "=",digit1 * digit2)
+    elif operation == "/" and digit2 != 0:
+        print(digit1,operation,digit2, "=",digit1 / digit2)
+    else:
+        print("incorrect input data")
+        if operation == "/" and digit2 == 0:
+            print("the divisor cannot be zero")
+    WhileY = input("run the calculations again y/n  ")
